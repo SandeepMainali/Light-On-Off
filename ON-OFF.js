dc3450e26f1750b59d69279923b615090c33cd18
Sandeep mainali <amanshrestha284@gmail.com>
2022-08-11 19:25:38 +0545

    second

--- ON-OFF.js
+++ ON-OFF.js
@@ -5,7 +5,7 @@
 
 <button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
 
-<img id="myImage" src="pic_bulboff.gif" style="width:300px">
+<img id="myImage" src="pic_bulboff.gif" style="width:100px">
 
 <button onclick="document.getElementById('myImage').src='pic_bulboff.gif'">Turn off the light</button>
 
